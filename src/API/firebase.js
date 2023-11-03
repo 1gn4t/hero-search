587b44435d3abe9409b435f6712f30ce52704740
Ignat <ignat.vet18@gmail.com>
2023-11-03 18:41:15 +0200

    fix firebase

---
+++ src/API/firebase.js
@@ -0,0 +1,48 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA0X5-77ZIzJW3UICz9P51IIMVBD6WsGOA",
+  authDomain: "super-hero-4f3c5.firebaseapp.com",
+  projectId: "super-hero-4f3c5",
+  storageBucket: "super-hero-4f3c5.appspot.com",
+  messagingSenderId: "915189083120",
+  appId: "1:915189083120:web:c30e58b2954b441c188b35",
+};
+
+initializeApp(firebaseConfig);
+
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const createUser = async (email, password) => {
+  const response = await createUserWithEmailAndPassword(auth, email, password);
+  return response.user;
+};
+
+export const singInUser = async (email, password) => {
+  const response = await signInWithEmailAndPassword(auth, email, password);
+  return response.user;
+};
+
+export const singInGoogle = async () => {
+  signInWithPopup(auth, provider).then(console.log);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedUser = (callback) =>
+  onAuthStateChanged(auth, callback);
