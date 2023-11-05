50e96413065e74933b4836461cacf0f23bf02e50
Ignat <ignat.vet18@gmail.com>
2023-11-05 15:13:04 +0200

    redux-thunk

--- src/store/search/search.actions.js
+++ src/store/search/search.actions.js
@@ -1,11 +1,32 @@
+import { PostService } from "../../API/PostService";
 import { createAction } from "../../utils/reducer";
 import { SEARCH_ACTION_TYPES } from "./search.types";
 
 export const setSearchString = (value) =>
   createAction(SEARCH_ACTION_TYPES.SET_SEARCH_STRING, value);
 
-export const setHerosMap = (value) =>
-  createAction(SEARCH_ACTION_TYPES.SET_HEROS_MAP, value);
-
 export const setCurrentHero = (value) =>
   createAction(SEARCH_ACTION_TYPES.SET_CURRENT_HERO, value);
+
+export const setHerosMapStart = () =>
+  createAction(SEARCH_ACTION_TYPES.SET_HEROS_MAP_STARTED);
+
+export const setHerosMapSuccess = (value) =>
+  createAction(SEARCH_ACTION_TYPES.SET_HEROS_MAP_SUCCESS, value);
+
+export const setHerosMapFailure = (value) =>
+  createAction(SEARCH_ACTION_TYPES.SET_HEROS_MAP_FAILURE, value);
+
+export const setHerosMap = (searchStr) => {
+  return async (dispatch) => {
+    try {
+      dispatch(setHerosMapStart());
+
+      const response = await PostService.getHero(searchStr);
+
+      dispatch(setHerosMapSuccess(response));
+    } catch (error) {
+      dispatch(setHerosMapFailure(error.message));
+    }
+  };
+};
